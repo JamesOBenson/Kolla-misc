50f5dd2c933671fb525105c203b58c8be23c048f
James Benson <James.o.benson@gmail.com>
2017-06-12 18:12:16 +0000

    updates

--- setup_kolla.sh
+++ setup_kolla.sh
@@ -4,7 +4,7 @@
 #  - SSH access with root user is possible
 #  - /etc/hosts file has all hostnames in it
 
-function local_docker_repo () {
+function create_docker_repo_and_images () {
   echo ""
   echo ""
   echo ""
@@ -20,7 +20,21 @@ function local_docker_repo () {
       -v registry:/var/lib/registry \
       registry:2
   kolla-build -t source -b ubuntu --registry 127.0.0.1:4000 --push
-  
+}
+
+function delete_docker_repo_and_images () {
+  echo ""
+  echo ""
+  echo ""
+  echo "DELETING the local docker repo and images..."
+  echo ""
+  echo ""
+  echo ""
+  sleep 5
+  docker rmi -f `docker images | grep kolla | awk '{print $3}'`
+  docker rmi `docker images | grep ubuntu | awk '{print $3}'`
+  docker stop `docker ps -a | grep registry | awk '{print $1}'`
+  docker rmi -f `docker images | grep registry | awk 'print $3}'`
 }
 
 
@@ -46,7 +60,6 @@ function settings () {
   cp -r /usr/local/share/kolla-ansible/etc_examples/kolla /etc/kolla/
 #cp /usr/local/share/kolla-ansible/ansible/inventory/* .
   cp globals.yml /etc/kolla/globals.yml
-
 }
 
 function bootstrap () {
@@ -59,6 +72,22 @@ function bootstrap () {
   kolla-ansible -i multinode bootstrap-servers
   kolla-genpwd
 }
+
+function destroy () {
+  ansible-playbook -i tmp kolla_bridge.yml --tags "kill_VMs"
+  kolla-ansible -i multinode destroy --yes-i-really-really-mean-it
+  ansible-playbook -i tmp  kolla_bridge.yml --tags "ceph"
+  while true; do
+    read -p "Do you with to delete ALL containers on hosts too?" yn
+    case $yn in
+      [Yy]* ) ansible-playbook -i tmp kolla_bridge.yml --tags "delete_images"; break;;
+      [Nn]* ) exit;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+}
+
+
 
 function prechecks () {
   echo ""
@@ -111,6 +140,14 @@ function usage () {
     echo "  post_deploy"
     echo "    runs the post-deploy and cat's out admin password"
     echo "  "
+    echo "  destroy"
+    echo "    destroys the evironment and wipes ceph"
+    echo "  "
+    echo "  delete_docker_repo_and_images"
+    echo "    deletes all docker images"
+    echo "  "
+    echo "  create_docker_repo_and_images"
+    echo "    creates the docker repo and builds images"
     echo "  "
     echo "  deploy_all"
     echo "    precheck"
@@ -154,6 +191,15 @@ function main () {
             ;;
         "post_deploy")
             post_deploy
+            ;;
+        "destroy")
+            destroy
+            ;;
+        "delete_docker_repo_and_images")
+            delete_docker_repo_and_images
+            ;;
+        "create_docker_repo_and_images")
+            create_docker_repo_and_images
             ;;
         esac
     fi
