52c13bbbcabcc7b0c2c9eaf6c2b7eb8226d4753b
James Benson <James.o.benson@gmail.com>
2017-06-22 14:05:26 -0500

    big fixes

--- setup_kolla.sh
+++ setup_kolla.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SLEEP=5
-
+TMP_INVENTORY_FILE="tmp6"
+INVENTORY_FILE="multinode6"
 # Verify:
 #  - SSH access with root user is possible
 #  - /etc/hosts file has all hostnames in it
@@ -33,12 +34,17 @@ function delete_docker_repo_and_images () {
   echo ""
   echo ""
   sleep $SLEEP
+  docker kill `docker ps -a | awk '{print $1}'`
+  docker rmi -f `docker images | grep none | awk '{print $3}'`
   docker rmi -f `docker images | grep kolla | awk '{print $3}'`
   docker rmi `docker images | grep ubuntu | awk '{print $3}'`
   docker stop `docker ps -a | grep registry | awk '{print $1}'`
-  docker rmi -f `docker images | grep registry | awk 'print $3}'`
+  docker rmi -f `docker images | grep registry | awk '{print $3}'`
 }
 
+function Reboot () {
+  ansible-playbook -i $INVENTORY_FILE  main.yml --tags "Reboot"
+}
 
 function one_time () {
   echo ""
@@ -50,10 +56,12 @@ function one_time () {
   apt install -y python-pip
   pip install -U pip
   apt install -y python-dev libffi-dev gcc libssl-dev
-  pip install -U ansible
-  pip install -U git+https://github.com/openstack/kolla-ansible.git@stable/ocata
+  pip install -U ansible==2.3.0.0
+  #pip install -U git+https://github.com/openstack/kolla-ansible.git@stable/ocata
+  pip install kolla-ansible==4.0.2
   curl -sSL https://get.docker.io | bash
-  pip install -U git+https://github.com/openstack/kolla.git@stable/ocata
+  pip install kolla==4.0.2
+  #pip install -U git+https://github.com/openstack/kolla.git@stable/ocata
   cp -r /usr/local/share/kolla-ansible/etc_examples/kolla /etc/kolla/
 }
 
@@ -74,11 +82,16 @@ function bootstrap () {
   echo ""
   echo " Sets up ceph, kolla bootstrap, and genpwd"
   echo ""
-  ansible-playbook -i tmp  kolla_bridge.yml --tags "oneTime,interface"
-  ansible-playbook -i tmp  kolla_bridge.yml --tags "ceph"
-#  ansible-playbook -i tmp  kolla_bridge.yml --tags reboot
-  kolla-ansible -i multinode bootstrap-servers
+  ansible-playbook -i $TMP_INVENTORY_FILE  main.yml --tags "oneTime" -u ubuntu --extra-vars='{"CIDR": "0.0.0.0"}'
+  ansible-playbook -i $TMP_INVENTORY_FILE  main.yml --tags "generate_public_interfaces" -u ubuntu
+  ansible -i $INVENTORY_FILE -m shell -a "parted /dev/sdb -s -- mklabel gpt mkpart KOLLA_CEPH_OSD_BOOTSTRAP 1 -1" storage 
+  ansible-playbook -i "$INVENTORY_FILE" main.yml --tags "Reboot" --extra-vasrs='{"CIDR":"0.0.0.0"}'
+  #ansible-playbook -i $TMP_INVENTORY_FILE  main.yml --tags "ceph" -u ubuntu --extra-vars='{"CIDR": "0.0.0.0"}'
   kolla-genpwd
+  kolla-ansible -i $INVENTORY_FILE bootstrap-servers
+  kolla-ansible certificates
+  ansible-playbook -i "$INVENTORY_FILE" main.yml --tags "destroy_public_interfaces" --extra-vasrs='{"CIDR":"0.0.0.0"}'
+  ansible-playbook -i "$INVENTORY_FILE" main.yml --tags "Reboot" --extra-vasrs='{"CIDR":"0.0.0.0"}'
 }
 
 function destroy () {
@@ -90,13 +103,17 @@ function destroy () {
   echo "- Destroy ceph volumes"
   echo "- optional: delete images on nodes"
   echo ""
-  ansible-playbook -i tmp kolla_bridge.yml --tags "kill_VMs"
-  kolla-ansible -i multinode destroy --yes-i-really-really-mean-it
-  ansible-playbook -i tmp  kolla_bridge.yml --tags "ceph"
+  ansible-playbook -i "$TMP_INVENTORY_FILE" kolla_bridge.yml --tags "kill_VMs" --extra-vars='{"CIDR":"0.0.0.0"}'
+  kolla-ansible -i "$INVENTORY_FILE" destroy --yes-i-really-really-mean-it
+  #ansible -i $INVENTORY_FILE -m shell -a "parted /dev/sdb -s -- mklabel gpt mkpart KOLLA_CEPH_OSD_BOOTSTRAP 1 -1" storage
+  ansible -i $INVENTORY_FILE -m shell -a "dd if=/dev/zero of=/dev/sdb count=1000 bs=1M" storage
+  ansible -i $INVENTORY_FILE -m shell -a "parted /dev/sdb -s -- mklabel gpt mkpart KOLLA_CEPH_OSD_BOOTSTRAP 1 -1" storage
+  ansible-playbook -i "$INVENTORY_FILE" main.yml --tags "Reboot" --extra-vars='{"CIDR":"0.0.0.0"}'
+  #ansible-playbook -i "$INVENTORY_FILE"  main.yml --tags "ceph" --extra-vars='{"CIDR":"0.0.0.0"}'
   echo "Do you with to delete ALL containers on hosts too?"
   select yn in "Yes" "No"; do
     case $yn in
-        Yes ) ansible-playbook -i tmp kolla_bridge.yml --tags "delete_images"; break;;
+        Yes ) ansible-playbook -i "$INVENTORY_FILE" main.yml --tags "nuke_it" --extra-vasrs='{"CIDR":"0.0.0.0"}'; break;;
         No ) exit;;
     esac
   done
@@ -108,18 +125,18 @@ function prechecks () {
   echo ""
   echo " Kolla prechecks and pull images local"
   echo ""
-  kolla-ansible prechecks -i multinode
-  sleep $SLEEP
-  kolla-ansible pull -i multinode
-  sleep $SLEEP
+  kolla-ansible prechecks -i  "$INVENTORY_FILE"
+  sleep "$SLEEP"
+  kolla-ansible pull -i "$INVENTORY_FILE"
+  sleep "$SLEEP"
 }
 
 function deploy () {
   echo ""
   echo "<deploy>"
   echo ""
-  kolla-ansible deploy -i multinode -vv
-  sleep $SLEEP
+  kolla-ansible deploy -i "$INVENTORY_FILE" -vv
+  sleep "$SLEEP"
 }
 
 function post_deploy () {
@@ -144,6 +161,9 @@ function usage () {
     echo "  settings"
     echo "    Moves local global.yml to /etc/kolla/global.yml"
     echo ""
+    echo "  Reboot"
+    echo "    Reboot all nodes"
+    echo ""
     echo "  bootstrap"
     echo "    Fixes interfaces file for bridges"
     echo "    bootstraps the servers"
@@ -154,6 +174,8 @@ function usage () {
     echo "    pulls images based off of globals file"
     echo "    "
     echo "  deploy"
+    echo "    Destroy public interfaces besides controllers"
+    echo "    Deploy"
     echo ""
     echo "  post_deploy"
     echo "    runs the post-deploy and cat's out admin password"
@@ -215,6 +237,9 @@ function main () {
             ;;
         "create_docker_repo_and_images")
             create_docker_repo_and_images
+            ;;
+        "Reboot")
+            Reboot
             ;;
         esac
     fi
